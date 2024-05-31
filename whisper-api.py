e9aeee46dcd94ab2534a4a0b9c1415c07cbd296a
usman <usmansiddiqui316@gmail.com>
2024-05-31 19:44:07 +0500

    added get route

--- whisper-api.py
+++ whisper-api.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
 from faster_whisper import WhisperModel
 import shutil
 import os
@@ -8,6 +7,10 @@ app = FastAPI()
 
 model_size = "large-v3"
 model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the transcription API"}
 
 @app.post("/transcribe/")
 async def transcribe_audio(file: UploadFile = File(...)):
@@ -19,5 +22,4 @@ async def transcribe_audio(file: UploadFile = File(...)):
     # Remove the temporary file
     os.remove("temp_audio.mp3")
     text = ' '.join(segment.text for segment in segments)
-
     return text
